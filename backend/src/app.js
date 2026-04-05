6c1d571d5d2c6c38930c073cd6308994047deda9
Harpreet Singh <harpreetsingh02092005@gmail.com>
2026-04-06 02:22:27 +0530

    trying to manage to get it deployed test_1\

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }), // to allow cross-origin requests from thr url and allow it to send cookies
 );
